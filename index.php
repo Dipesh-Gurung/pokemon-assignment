33b389b54468c5e8e5d209e803afdb434a70df08
Dipesh-Gurung <128246498+Dipesh-Gurung@users.noreply.github.com>
2024-08-01 23:56:33 +0545

    The final verdict of change is this

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$json = file_get_contents("./data.json");
+$data = json_decode($json, true);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,19 +13,48 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script>
+    function collectPokemon(button) {
+      button.innerText = "Collected";
+      button.disabled = true;
+    }
 
+    function filterPokemon() {
+      const searchInput = document.getElementById("searchInput").value.toLowerCase();
+      const tableBody = document.getElementById("pokemonTableBody");
+      const rows = tableBody.getElementsByTagName("tr");
+
+      for (let i = 0; i < rows.length; i++) {
+        const nameCell = rows[i].getElementsByTagName("td")[1];
+        if (nameCell) {
+          const name = nameCell.textContent || nameCell.innerText;
+          if (name.toLowerCase().indexOf(searchInput) > -1) {
+            rows[i].style.display = "";
+          } else {
+            rows[i].style.display = "none";
+          }
+        }
+      }
+    }
+  </script>
 </head>
 
-<body class="bg-dark">
+<body class="bg-black">
   <div class="container">
     <div class="mt-4 mb-5 d-flex justify-content-between align-items-center">
       <h1 class="text-white">Get your Pokemon!</h1>
       <div>
-        <button class="btn btn-primary">
-          <i class="fa fa-sign-in"></i> Login</button>
+        <form class="d-flex" role="search" onsubmit="return false;">
+          <input id="searchInput" class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onkeyup="filterPokemon()">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <i class="fab fa-youtube" style="color: red;"></i>
+        <i class="fab fa-instagram" style="color: red;"></i>
+        <i class="fab fa-whatsapp" style="color: green;"></i>
+        <button class="btn btn-warning"><i class="fa fa-sign-in"></i> Login</button>
       </div>
     </div>
-    <table class="table table-dark">
+    <table class="table table-dark table-striped">
       <thead>
         <tr>
           <th scope="col">Image</th>
@@ -32,22 +66,24 @@
           <th scope="col">Action</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Bulbasaur"></td>
-          <td>Bulbasaur</td>
-          <td>Seed Pokémon</td>
-          <td>Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.</td>
-          <td>6.9 kg</td>
-          <td>0.7 m</td>
-          <td><button class="btn btn-success">Catch</button></td>
-        </tr>
+      <tbody id="pokemonTableBody">
+        <?php foreach ($data as $element) : ?>
+          <tr>
+            <td><img src="<?= $element["image"]["thumbnail"] ?>" alt=""></td>
+            <td><?= $element["name"]["english"] ?></td>
+            <td><?= strtoupper($element["species"]) ?></td>
+            <td><?= $element["description"] ?></td>
+            <td><?= $element["profile"]["weight"] ?></td>
+            <td><?= $element["profile"]["height"] ?></td>
+            <td><button class="btn btn-warning" onclick="collectPokemon(this)">Collect</button></td>
+          </tr>
+        <?php endforeach ?>
       </tbody>
     </table>
   </div>
+  <!-- DNT MAKE ANY CHANGES ON THE CODE BELOW -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-  </script>
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
